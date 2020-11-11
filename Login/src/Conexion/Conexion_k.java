3c595c1fd05ff7974a70546903b47a6d97542ba6
KEVIN LOPEZ MEZA <kevin.lopez3@ulatina.net>
2020-11-11 11:36:02 -0600

    Add clase conexion_k

---
+++ Login/src/Conexion/Conexion_k.java
@@ -0,0 +1,35 @@
+package Conexion;
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion_k {
+    
+     Connection conectar=null;
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection("jdbc:mysql://localhost/bd_repuestos"+
+                    "?userTimezone=true&serverTimezone=UTC","root","");
+            
+            
+           System.out.print("Conexion exitosa...");
+        } catch (Exception e) {
+           
+        }
+        return conectar;
+    }
+    //Por aquello....
+     public void Desconectar(){
+        conectar=null;
+        if(conectar==null){
+            System.out.println("Conexion terminada...");
+        }
+    }
+    
+    
+}
